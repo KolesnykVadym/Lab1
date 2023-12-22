30a3241a0392a440ddf7a7f7cebf008e560ae2fd
KolesnykVadym <kolesnikv783@gmail.com>
2023-12-22 19:53:14 +0200

    Адаптив lab1

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
-
+document.querySelector('.header-burger').addEventListener('click', function(){
+   this.classList.toggle('active')
+   document.querySelector('.navbar').classList.toggle('open')
+})
 
 window.onscroll = () => {
    let header = document.querySelector('header');
